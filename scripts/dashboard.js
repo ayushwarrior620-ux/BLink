cb9214087315f67b16fb554361d8ca385e5690be
vAayush3301 <vaayush3301@gmail.com>
2026-05-17 23:58:19 +0530

    Login and Logout

---
+++ scripts/dashboard.js
@@ -0,0 +1,68 @@
+import { auth }
+from "./firebase.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "firebase/auth";
+
+
+const nameElement =
+    document.getElementById("name");
+
+const logoutBtn =
+    document.getElementById("logoutBtn");
+
+
+onAuthStateChanged(
+    auth,
+
+    async (user) => {
+
+        if (!user) {
+            window.location.href =
+                "/login.html";
+
+            return;
+        }
+
+        try {
+            const token =
+                await user.getIdToken();
+
+            const response =
+                await fetch(
+                    "https://blink-g8w4.onrender.com/users/me",
+                    {
+                        headers: {
+                            Authorization:
+                                `Bearer ${token}`
+                        }
+                    }
+                );
+
+            const data =
+                await response.json();
+
+            nameElement.textContent =
+                data.name;
+
+        } catch (error) {
+
+            console.error(error);
+        }
+    }
+);
+
+
+logoutBtn.addEventListener(
+    "click",
+
+    async () => {
+
+        await signOut(auth);
+
+        window.location.href =
+            "/login.html";
+    }
+);
--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,60 @@
-import { initializeApp } from "firebase/app";
+import { auth }
+from "./firebase.js";
 
 import {
-    getAuth,
     GoogleAuthProvider,
     signInWithPopup
 } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC1Xm5xjv31dPz1XS0jN6KMycJmInuPAX4",
-  authDomain: "arr-3301.firebaseapp.com",
-  projectId: "arr-3301",
-  storageBucket: "arr-3301.firebasestorage.app",
-  messagingSenderId: "567076914671",
-  appId: "1:567076914671:web:aa31a650ffb73846afe21a",
-  measurementId: "G-7TNRNVHDBT"
-};
 
-const app = initializeApp(firebaseConfig);
+const provider =
+    new GoogleAuthProvider();
 
-const auth = getAuth(app);
 
-const provider = new GoogleAuthProvider();
-
-const btn =
+const loginBtn =
     document.getElementById("googleLoginBtn");
 
-btn.addEventListener("click", async () => {
 
-    try {
+loginBtn.addEventListener(
+    "click",
+    async () => {
 
-        const result =
-            await signInWithPopup(auth, provider);
+        try {
+            const result =
+                await signInWithPopup(
+                    auth,
+                    provider
+                );
 
-        const user = result.user;
+            const user =
+                result.user;
 
-        const token =
-            await user.getIdToken();
+            const token =
+                await user.getIdToken();
 
-        console.log(token);
+            const response =
+                await fetch(
+                    "https://blink-g8w4.onrender.com/users/me",
+                    {
+                        headers: {
+                            Authorization:
+                                `Bearer ${token}`
+                        }
+                    }
+                );
 
-        const response = await fetch(
-            "https://blink-g8w4.onrender.com/users/me",
-            {
-                method: "GET",
+            if (!response.ok) {
 
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
+                throw new Error(
+                    "Backend auth failed"
+                );
             }
-        );
 
-        const data = await response.json();
-        if (response.ok) {
-            window.location.href = "/index.html"
+            window.location.href =
+                "/index.html";
+
+        } catch (error) {
+            console.error(error);
         }
-    } catch (error) {
-        console.error(error);
     }
-});
+);
